788138f3e5fd796a0c52eb034644ec265da45d53
iammiro <miroslav.kolomiets@gmail.com>
2017-04-13 20:44:25 +0300

    first commit

---
+++ sw.js
@@ -0,0 +1,34 @@
+this.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('v1').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/app.js',
+                '/manifest.json',
+                '/star-wars-logo.jpg',
+                '/gallery/bountyHunters.jpg',
+                '/gallery/snowTroopers.jpg',
+                '/gallery/myLittleVader.jpg'
+            ]);
+        })
+    );
+});
+
+this.addEventListener('fetch', function(event) {
+    var response;
+    event.respondWith(
+        caches.match(event.request).catch(function() {
+            return fetch(event.request);
+        }).then(function(r) {
+            response = r;
+            caches.open('v1').then(function(cache) {
+                cache.put(event.request, response);
+            });
+            return response.clone();
+        }).catch(function() {
+            return caches.match('/gallery/myLittleVader.jpg');
+        })
+    );
+});
